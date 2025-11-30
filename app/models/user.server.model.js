cb3775c925cdc1164d0b13368f0724ecc5a5f26d
Ayman-ss <ayman.sohail@ontariotechu.net>
2025-11-29 23:54:50 -0500

    pushing edits

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,23 +1,29 @@
-// user model for signup / signin
-
-
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+  // Local signup fields (optional so Google/GitHub won't fail)
   firstName: { type: String },
   lastName: { type: String },
   username: { type: String },
   password: { type: String },
 
-  // For OAuth
+  // OAuth login fields
   googleId: { type: String },
   githubId: { type: String },
 
+  // Shared fields
   displayName: { type: String },
   email: { type: String },
-});
 
+}, { timestamps: true });
+
+// This prevents login errors when Google/GitHub tries to create a user
+// without username/password.
+UserSchema.index(
+  { googleId: 1, githubId: 1, username: 1 },
+  { unique: false }
+);
 
 module.exports = mongoose.model('User', UserSchema);
 
-//ends
+
